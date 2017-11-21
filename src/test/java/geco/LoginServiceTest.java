18db3f056d8709f49bc4a69531b78bb026994da1
mathieulievre <mathieulievre0@gmail.com>
2017-11-21 11:10:05 +0100

    #1 Creation du squelette de tests

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,22 @@
+package geco;
+
+import org.junit.Test;
+
+public class LoginServiceTest {
+    @Test
+    public void loginExists() throws Exception {
+    }
+
+    @Test
+    public void addLogin() throws Exception {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() throws Exception {
+    }
+
+    @Test
+    public void findAllLogins() throws Exception {
+    }
+
+}
