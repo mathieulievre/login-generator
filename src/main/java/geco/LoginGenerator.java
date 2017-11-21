47f0b69627bcd265d01e7a68d9a87aad0582c976
mathieulievre <mathieulievre0@gmail.com>
2017-11-21 17:21:29 +0100

    close #2 couverture 100%, fix #3

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -12,6 +12,7 @@ public class LoginGenerator {
 
     /**
      * Construit un login generator
+     *
      * @param loginService le service de login
      */
     public LoginGenerator(LoginService loginService) {
@@ -24,21 +25,23 @@ public class LoginGenerator {
      * par rapport a la liste des logins existants. En cas de doublon, on incremente le doublon d'un indice. Ci dessous des
      * exemples :
      * <ul>
-     *     <li>Paul Dupond -> PDUP </li>
-     *     <li>Pierre Dupard -> PDUP1</li>
-     *     <li>Jacques Durand -> JDUR</li>
-     *     <li>Lionel R&eacute;gal -> LREG</li>
+     * <li>Paul Dupond -> PDUP </li>
+     * <li>Pierre Dupard -> PDUP1</li>
+     * <li>Jacques Durand -> JDUR</li>
+     * <li>Lionel Régal -> LREG</li>
      * </ul>
-     * @param nom le nom
+     *
+     * @param nom    le nom
      * @param prenom le prenom
      * @return le login genere
      */
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
-        String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
-        String login = p+n ;
+        String p = deAccent(prenom.substring(0, 1).toUpperCase());
+        String n = deAccent(nom.substring(0, 3).toUpperCase());
+        String login = p + n;
         if (loginService.loginExists(login)) {
-            login = login + "1" ;
+            int suffixe = loginService.findAllLoginsStartingWith(login).size();
+            login = login + suffixe;
         }
         loginService.addLogin(login);
         return login;
@@ -55,10 +58,6 @@ public class LoginGenerator {
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
         return pattern.matcher(nfdNormalizedString).replaceAll("");
     }
-
-
-
-
 
 
 }
